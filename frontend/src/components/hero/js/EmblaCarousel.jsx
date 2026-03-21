21bf92f5dd5a07159a519a349af4c20e267187ab
Brenisdone <fernandes.brendon69@gmail.com>
2026-03-21 21:24:49 +0530

    added dynamic gradient overlay

--- frontend/src/components/hero/js/EmblaCarousel.jsx
+++ frontend/src/components/hero/js/EmblaCarousel.jsx
@@ -1,3 +1,4 @@
+import { useEffect,useState,useRef } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Fade from 'embla-carousel-fade'
 import {
@@ -16,8 +17,16 @@ import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 import {Link,useNavigate} from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
+import {getColor} from 'colorthief';
+
+
 const EmblaCarousel = ({ movies, options }) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Fade()])
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Fade()]);
+
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const [currentSlideColor, setCurrentSlideColor] = useState(null);
+  const slideNodesRef = useRef([]);
+
 
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
     useDotButton(emblaApi)
@@ -35,16 +44,71 @@ const EmblaCarousel = ({ movies, options }) => {
     navigate(`/Reviews/${movieId}`);
   }
 
+  //being used to get index of currently being displayed slide
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    const onSelect = () => {
+      const index = emblaApi.snapIndex(0);
+      setCurrentSlideIndex(index); // current slide
+      console.log("New Slide: ",index);
+    };
+
+    emblaApi.on('select',onSelect);
+
+    // Set initial slide
+    setCurrentSlideIndex(emblaApi.snapIndex(0));
+
+    return () => emblaApi.off('select', onSelect);
+  }, [emblaApi]);
+
+  useEffect(() => {
+    console.log("React state updated:", currentSlideIndex);
+  }, [currentSlideIndex]);
+
+
+  //calculate div with class=embla_slide
+  useEffect(() => {
+    if (!emblaApi || !movies?.length) return;
+
+    if (slideNodesRef.current.length === 0) {
+      slideNodesRef.current = emblaApi.slideNodes();
+      console.log("slideNodes initialized:", slideNodesRef.current);
+    }
+  }, [emblaApi, movies]);
+
+
+  // Compute Color Thief only when current slide changes
+  useEffect(() => {
+    if (!slideNodesRef.current.length) return;
+
+    const imgEl = slideNodesRef.current[currentSlideIndex]
+      ?.querySelector('img.embla__slide__img');
+
+    getColor(imgEl).then(color => {
+      setCurrentSlideColor(color); // color is now [R, G, B]
+      console.log("Dominant color:", color);
+    });
+  }, [currentSlideIndex,slideNodesRef.current]);
+
+
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {movies?.map((movie) => (
-            <div className="embla__slide" key={movie.imdbId}>
+            <div className="embla__slide" key={movie.imdbId}
+              style={{
+                '--gradient-color': currentSlideColor
+                  ? `rgb(${currentSlideColor._r}, ${currentSlideColor._g}, ${currentSlideColor._b})`
+                  : '#2F70C1'
+              }}          
+            >
               <img
                 className="embla__slide__img"
                 src={movie.backdrops[0]}
                 alt={movie.title}
+                crossOrigin="anonymous"
               />
               <div className='gradient_overlay'></div>
               <div className='movie-detail'>
@@ -58,7 +122,7 @@ const EmblaCarousel = ({ movies, options }) => {
                   </div>
 
                   <div className='movie-review-button-container'>
-                    <Button variant="info" onClick={()=>reviews(movie.imdbId)}>
+                    <Button variant="info" className='movie-review-button' onClick={()=>reviews(movie.imdbId)}>
                       Reviews
                     </Button>
                   </div>
